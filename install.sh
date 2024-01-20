c5aacb197bc03c47db7d2523794a5efbd432374d
Peiqi Liu <leo20021210@163.com>
2024-01-20 17:48:55 -0500

    fix installation

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 mamba create -n home_engine python=3.10
 mamba activate home_engine
-mamba install -y -c "nvidia/label/cuda-11.8.0" -c pytorch -c nvidia cuda-toolkit pytorch torchvision torchaudio
+#mamba install -y -c "nvidia/label/cuda-11.8.0" -c pytorch -c nvidia cuda-toolkit pytorch torchvision torchaudio
+mamba install -y -c "nvidia/label/cuda-11.8.0" -c pytorch cuda-toolkit pytorch torchvision torchaudio
 python -m pip install -r requirements.txt
 cd clip-fields/gridencoder/
 python setup.py install
 cd ../..
-cd lerf
-python -m pip install -e .
-ns-install-cli
-cd ..
 cd usa
 pip install -e .
